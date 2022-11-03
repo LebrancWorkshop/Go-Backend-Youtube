67892f044b5df07f7d398a12a8750a490dc3794a
lebrancconvas <lebrancconvas@gmail.com>
2022-11-03 15:08:56 +0700

    ✨ feat: get environment variables

--- bookborrow/app/main.go
+++ bookborrow/app/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
-	
+	// Get Environtment Variables. 
+	dialect := os.Getenv("DIALECT");
+	host := os.Getenv("HOST");
+	port := os.Getenv("PORT");
+	user := os.Getenv("USER");
+	password := os.Getenv("PASSWORD");
+	name := os.Getenv("NAME");
 }
